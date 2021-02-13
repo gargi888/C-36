82f0f8dfd4477b2fd7df02319aeeec34ad91e1cf
gargi888 <74598857+gargi888@users.noreply.github.com>
2021-02-13 10:44:40 +0530

    Add files via upload

---
+++ js/Form.js
@@ -0,0 +1,25 @@
+class Form{
+    constructor(){
+
+    }
+    display(){
+        var title = createElement('h2');
+        title.html("Car Racing Game");
+        title.position(130,0);
+        var input = createInput("Name");
+        var button = createButton('Play');
+        var greeting = createElement('h3');
+        input.position(380,160);
+        button.position(380,200);
+        button.mousePressed(function(){
+            input.hide();
+            button.hide();
+            var name = input.value();
+            playerCount+=1;
+            player.update(name);
+            player.updateCount(playerCount);
+            greeting.html("HELLO" + name);
+            greeting.position(400,160);
+        });
+    }
+}
